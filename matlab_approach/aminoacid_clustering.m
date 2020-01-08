7a26ef343c847e2290c9b8a3ca8bda2b3dc17482
Higor Coimbra <higorcoimbra@gmail.com>
2020-01-07 21:12:41 -0300

    format result matrix to make statistics report

--- matlab_approach/aminoacid_clustering.m
+++ matlab_approach/aminoacid_clustering.m
@@ -5,6 +5,11 @@ distance_matrix = fscanf(fileID, formatSpec, [67, 16383])';
 fclose(fileID);
 
 %execute k-means
-f = @() kmeans(distance_matrix(:, (2:67)), 500, 'Start', 'sample');
-timeit(f)
+idx = kmeans(distance_matrix(:, (2:67)), 500, 'Start', 'sample');
+
+%create matrix to map each id file to the associated cluster
+map_id_to_cluster = cat(2, distance_matrix(:,1), idx);
+
+%group each cluster number on subsequent rows in a matrix
+
 
