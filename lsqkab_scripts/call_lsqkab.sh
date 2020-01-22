8df7bf042376566f9005ebe9d33ef285beda2985
Higor Coimbra <higorcoimbra@gmail.com>
2020-01-21 21:57:13 -0300

    add delta files location to lsqkab shell script

--- lsqkab_scripts/call_lsqkab.sh
+++ lsqkab_scripts/call_lsqkab.sh
@@ -2,16 +2,19 @@
 
 export START_CCP4_FILE_LOCATION=/home/higor/Documents/CCP4/ccp4-7.0
 export PDB_DATABASE=/home/higor/Documents/CEFET/TCC/parallel-k-means/protein_base/mc6
-
-./$START_CCP4_FILE_LOCATION/start
+export DELTA_FILES_LOCATION=/home/higor/Documents/CEFET/TCC/parallel-k-means/protein_base/delta_files
+source ~/Documents/CCP4/ccp4-7.0/bin/ccp4.setup-sh
 
 moved_file=`cat $1 | egrep ^$2 | cut -d ' ' -f 2`
 fixed_file=`cat $1 | egrep ^$3 | cut -d ' ' -f 2`
 
-lsqkab\
-XYZINM $PDB_DATABASE/$moved_file\
-XYZINF $PDB_DATABASE/$fixed_file\
-DELTAS $PDB_DATABASE/$moved_file-$fixedfile.delta\
+echo $moved_file
+echo $fixed_file
+
+lsqkab                        \
+XYZINM $PDB_DATABASE/${moved_file} \
+XYZINF $PDB_DATABASE/${fixed_file} \
+DELTAS $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta \
 << 'END-lsqkab'
 FIT ATOM 1 to 12
 MATCH 1 to  12
