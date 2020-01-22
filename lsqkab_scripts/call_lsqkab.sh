43fbb280ddb2380ffe9eb09ab3237ca4b077abd1
Higor Coimbra <higorcoimbra@gmail.com>
2020-01-21 21:23:35 -0300

    script to run lsqkab with pdb files as arguments

--- lsqkab_scripts/call_lsqkab.sh
+++ lsqkab_scripts/call_lsqkab.sh
@@ -1,3 +1,18 @@
 #! /bin/bash
 
-cat map_test_file | egrep ^$1 | cut -d ' ' -f 2
+export PDB_DATABASE=/home/higor/Documents/CEFET/TCC/parallel-k-means/protein_base/mc6
+
+moved_file=`cat $1 | egrep ^$2 | cut -d ' ' -f 2`
+fixed_file=`cat $1 | egrep ^$3 | cut -d ' ' -f 2`
+
+lsqkab\
+XYZINM $PDB_DATABASE/$moved_file\
+XYZINF $PDB_DATABASE/$fixed_file\
+DELTAS $PDB_DATABASE/$moved_file-$fixedfile.delta\
+<< 'END-lsqkab'
+FIT ATOM 1 to 12
+MATCH 1 to  12
+OUTPUT DELTAS
+end
+END-lsqkab
+
