55af3c86857e531dfbc1809074dd6297854e770e
Higor Coimbra <higorcoimbra@gmail.com>
2020-02-01 15:26:20 -0300

    adjusting awk calculate of distance values on delta files

--- lsqkab_scripts/call_lsqkab.sh
+++ lsqkab_scripts/call_lsqkab.sh
@@ -8,13 +8,13 @@ source ~/Documents/CCP4/ccp4-7.0/bin/ccp4.setup-sh
 moved_file=`cat $1 | egrep ^$2 | cut -d ' ' -f 2`
 fixed_file=`cat $1 | egrep ^$3 | cut -d ' ' -f 2`
 
-echo $moved_file
-echo $fixed_file
+#echo $moved_file
+#echo $fixed_file
 
 lsqkab                        \
 XYZINM $PDB_DATABASE/${moved_file} \
 XYZINF $PDB_DATABASE/${fixed_file} \
-DELTAS $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta \
+DELTAS $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta > /dev/null \
 << 'END-lsqkab'
 FIT ATOM 1 to 12
 MATCH 1 to  12
@@ -22,6 +22,7 @@ OUTPUT DELTAS
 end
 END-lsqkab
 
-delta_file=`cat $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta | cut -d ' ' -f6`
-valid_superposition=`awk '$1>100{c++} END{print c+0}' $delta_file`
-echo $delta_file
+delta_file=`cat $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta | cut -d$' ' -f6`
+printf '%b\n' $delta_file > $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta
+valid_superposition=`awk '$1<0.5{c++} END{print c+0}' $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta`
+echo $valid_superposition
