ad4ec2c602b1026f705c152e9d7493dfafbdd43b
Higor Coimbra <higorcoimbra@gmail.com>
2020-02-17 22:00:29 -0300

    start of superposition code on matlab main script

--- matlab_approach/aminoacid_clustering.m
+++ matlab_approach/aminoacid_clustering.m
@@ -1,6 +1,6 @@
 %constants util
 number_of_files = 16383;
-number_of_clusters = 500;
+number_of_clusters = 100;
 data_column_start = 2;
 data_column_finish = 67;
 
@@ -20,5 +20,15 @@ map_file_id_to_cluster_id = cat(2, distance_matrix(:,1), idx);
 result_grouping = zeros(number_of_files, number_of_clusters);
 for i = 1:number_of_files
     result_grouping(i, map_file_id_to_cluster_id(i,2)) = map_file_id_to_cluster_id(i,1);  
+end
+
+%call the n-to-n superposition on LSQKAB
+path_to_lsqkab_script_execution = '../lsqkab_scripts/./call_lsqkab.sh ../lsqkab_scripts/map_file_id_to_file_name ';
+for i = 1:number_of_clusters
+    %get all ids on the column i who value is > 0
+    cluster_i_ids = result_grouping(result_grouping(:,i) > 0); 
+    %call script execution to j combine with the other k values
+    %[status, cmdout] = system(join);
+    %match_rms_criteria = str2double(cmdout);
 end
 
