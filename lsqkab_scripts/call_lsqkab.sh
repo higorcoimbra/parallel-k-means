5d8a545fdb6b107a584e912a8f321468b448d67c
Higor Coimbra <higorcoimbra@gmail.com>
2020-02-29 18:55:40 -0300

    fixing system argument reading on lsqkab call script

--- lsqkab_scripts/call_lsqkab.sh
+++ lsqkab_scripts/call_lsqkab.sh
@@ -5,8 +5,8 @@ export PDB_DATABASE=/home/higor/Documents/CEFET/TCC/parallel-k-means/protein_bas
 export DELTA_FILES_LOCATION=/home/higor/Documents/CEFET/TCC/parallel-k-means/protein_base/delta_files
 source ~/Documents/CCP4/ccp4-7.0/bin/ccp4.setup-sh
 
-moved_file=`cat $1 | egrep ^$2 | cut -d ' ' -f 2`
-fixed_file=`cat $1 | egrep ^$3 | cut -d ' ' -f 2`
+moved_file=`cat $1 | egrep -e "^\$2 "  | cut -d ' ' -f 2`
+fixed_file=`cat $1 | egrep -e "^\$3 " | cut -d ' ' -f 2`
 
 #echo $moved_file
 #echo $fixed_file
@@ -25,4 +25,5 @@ END-lsqkab
 delta_file=`cat $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta | cut -d$' ' -f6`
 printf '%b\n' $delta_file > $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta
 valid_superposition=`awk '$1<0.5{c++} END{print c+0}' $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta`
+#rm $DELTA_FILES_LOCATION/${moved_file}-${fixed_file}.delta
 echo $valid_superposition
