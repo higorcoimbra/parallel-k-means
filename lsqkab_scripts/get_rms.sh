92e60afd203e0bd4bff7a327049ef291fdfbe1c8
Higor Coimbra <higorcoimbra@gmail.com>
2020-06-02 18:54:42 -0300

    transform moved file in fixed file and vice versa on superposition

--- lsqkab_scripts/get_rms.sh
+++ lsqkab_scripts/get_rms.sh
@@ -1,2 +1,3 @@
 #! /bin/sh
 ../lsqkab_scripts/call_lsqkab.sh $1 $2 $3 $4 | egrep -e "RMS.*XYZ" | cut -d '=' -f2 >> ./rms_files/rms_results_cluster_$4
+../lsqkab_scripts/call_lsqkab.sh $2 $1 $3 $4 | egrep -e "RMS.*XYZ" | cut -d '=' -f2 >> ./rms_files/rms_results_cluster_$4
