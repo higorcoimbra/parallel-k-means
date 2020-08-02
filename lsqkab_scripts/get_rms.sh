80f5216c26e7efcfe9ac416dfb5d4c23bfa98017
Higor Coimbra <higorcoimbra@gmail.com>
2020-08-02 15:14:03 -0300

    get RMS measure from MAXIMUm lsqkab output attribute

--- lsqkab_scripts/get_rms.sh
+++ lsqkab_scripts/get_rms.sh
@@ -1,3 +1,2 @@
 #! /bin/sh
-../lsqkab_scripts/call_lsqkab.sh $1 $2 $3 $4 | egrep -e "RMS.*XYZ" | cut -d '=' -f2 >> ./rms_files/rms_results_cluster_$4
-../lsqkab_scripts/call_lsqkab.sh $1 $3 $2 $4 | egrep -e "RMS.*XYZ" | cut -d '=' -f2 >> ./rms_files/rms_results_cluster_$4
+../lsqkab_scripts/call_lsqkab.sh $1 $2 $3 $4 | egrep -e "MAXIMUM.*XYZ" | cut -d '=' -f2 >> ./rms_files/rms_results_cluster_$4
